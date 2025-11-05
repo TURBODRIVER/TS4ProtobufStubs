b41d6dfee841ccf64c911bbce6a55b1946e1f8e2
TURBODRIVER <wickedwoohoo@gmail.com>
2025-11-05 14:19:15 +1100

    1.119.109

--- protocolbuffers/Exchange_pb2.pyi
+++ protocolbuffers/Exchange_pb2.pyi
@@ -119,7 +119,7 @@ class TrayHouseholdMetadata(Message):
 class TrayMetadata(Message):
     class TrayMetadataVersion(IntEnum):
         v000: 'TrayMetadata.TrayMetadataVersion' = 0
-        currentVersion: 'TrayMetadata.TrayMetadataVersion' = 11800
+        currentVersion: 'TrayMetadata.TrayMetadataVersion' = 11900
 
     v000 = TrayMetadataVersion.v000
     currentVersion = TrayMetadataVersion.currentVersion
